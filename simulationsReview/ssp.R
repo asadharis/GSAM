f52708ab54179c239a60900aa810d481b753328c
asadharis <aharis@uw.edu>
2021-01-31 15:47:32 -0500

    Updated and improved simulation files for Reviewer Simulations2

--- simulationsReview/ssp.R
+++ simulationsReview/ssp.R
@@ -1,5 +1,5 @@
 
-# This function fits the TF penalty and returns a single
+# This function fits the SSP penalty and returns a single
 # value: the value of the oracle MSE.
 oracle.SPLINE <- function(dat,lambda.max = 1, lambda.min.ratio = 1e-2,
                       tol = 1e-4, zeta = NULL, ...) {
@@ -22,7 +22,7 @@ SimSPLINE <- function(dat, lambda.max = 1, lambda.min.ratio = 1e-2,
                   tol = 1e-4, ...) {
   require(GSAM)
 
-  # lambda.max = 1; lambda.min.ratio = 1e-2
+  # lambda.max = NULL; lambda.min.ratio = 1e-2
   # tol = 1e-4;max.iter = 300
 
   # First we fit the lambda, lambda^2 models
@@ -31,15 +31,12 @@ SimSPLINE <- function(dat, lambda.max = 1, lambda.min.ratio = 1e-2,
                     tol = tol, zeta = NULL, ...)
 
   zeta.len <- 10
-  mse.zeta <- numeric(zeta.len)
-  zeta.seq <- seq(1e-3, 1-1e-3, length = zeta.len)
-  for(i in 1:zeta.len) {
-    print(i)
-    mse.zeta[i] <- oracle.SPLINE(dat,lambda.max = lambda.max,
-                             lambda.min.ratio = lambda.min.ratio,
-                             tol = tol, zeta = zeta.seq[i],...)
 
-  }
+  zeta.seq <- seq(1e-3, 1-1e-5, length = zeta.len)
+  mse.zeta <- tryCatch(ssp_par(dat,lambda.max ,
+                                 lambda.min.ratio ,
+                                 tol, zeta.seq = zeta.seq,...),
+                         error = function(e) print(e))
 
   mse1 <- min(mse.zeta)
 
@@ -48,4 +45,27 @@ SimSPLINE <- function(dat, lambda.max = 1, lambda.min.ratio = 1e-2,
               "mse1" = mse1))
 }
 
+##########################################################
+######## Temp function for parallel computing ############
+##########################################################
 
+ssp_par <- function(dat,lambda.max ,
+                    lambda.min.ratio ,
+                    tol, zeta.seq = zeta.seq,...) {
+  n.zeta <- length(zeta.seq)
+  foreach(i = 1:n.zeta, .combine = c) %dopar% {
+    fit <- GSAM::fit.additive(y=dat$y, x=dat$x,
+                        family="gaussian", method = "sobolev",
+                        lambda.max = lambda.max,
+                        lambda.min.ratio = lambda.min.ratio,
+                        tol = tol, zeta = zeta.seq[i],...)
+
+    fit.vals <- apply(fit$f_hat, c(1,3),sum)
+    fit.vals <- fit.vals + fit$intercept
+
+    # Evaluate the MSE_true
+    mse.true <- colMeans((fit.vals - dat$f0)^2)
+
+    min(mse.true)
+  }
+}
